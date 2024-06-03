f1c8737b111a336c8a7981577cde383eec8ae4c0
Carl Astih <iMelancholy>
2024-06-04 07:06:28 +0800

    back button gone

--- resources/views/employ/edit.blade.php
+++ resources/views/employ/edit.blade.php
@@ -65,7 +65,6 @@
                         <div class="row">
                             <div class="col-md-12 mt-3">
                                 <button type="submit" class="btn btn-primary">Update</button>
-                                <!-- <a href="{{ url('employ') }}" class="btn btn-secondary">Back</a> -->
                             </div>
                         </div>
                     </form>
