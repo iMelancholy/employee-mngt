bab2d2349584036b1a612c2dce4410744b724081
GioViolanda <2021314404@dhvsu.edu.ph>
2024-06-04 14:06:01 +0800

    ye

--- resources/views/employ/index.blade.php
+++ resources/views/employ/index.blade.php
@@ -1,6 +1,22 @@
 @extends('layouts.app')
 
-@section('content')
+@section('content') 
+<div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Users list of users</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item active">Dashboard</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
 
     <div class="container">
 
